20992e78f8b4d7050fc3b5b3188979675fc81003
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2015-03-06 13:59:59 +0200

    Allow setting string array property

--- safearrayslices.go
+++ safearrayslices.go
@@ -16,3 +16,16 @@ func safeArrayFromByteSlice(slice []byte) *SafeArray {
 	}
 	return array
 }
+
+func safeArrayFromStringSlice(slice []string) *SafeArray {
+	array, _ := safeArrayCreateVector(VT_UI1, 0, uint32(len(slice)))
+
+	if array == nil {
+		panic("Could not convert []string to SAFEARRAY")
+	}
+
+	for i, v := range slice {
+		safeArrayPutElement(array, int64(i), uintptr(unsafe.Pointer(&v)))
+	}
+	return array
+}
